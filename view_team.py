14f9e3709d71ba14bb5d6dad56be470a84eee5fb
Reena200 <138387440+Reena200@users.noreply.github.com>
2023-07-17 15:40:39 +0530

    Add files via upload

---
+++ view_team.py
@@ -0,0 +1,7 @@
+# import view_team;
+
+def view_team():
+    f=open("data.txt","r")
+    username = [line.split(",")[0] for line in f]
+    for name in username:
+        print(f"Team members are: \n{name}")
