cf231691a0e83c695fafa9b1051d55fa610214e9
Reena200 <138387440+Reena200@users.noreply.github.com>
2023-07-17 15:39:16 +0530

    Add files via upload

---
+++ view_position.py
@@ -0,0 +1,8 @@
+# import user_register
+def view_pos():
+    f=open("data.txt","r+")
+    for line in f:
+        parts = line.split(",")
+        username = parts[0]
+        position = parts[2].strip()
+        print(f"Username: {username},\n Position: {position}")
